919e9eab362027cf9683bad0a6d92fffa8ad503a
blaise789 <alainiraguha2016@gmail.com>
2024-03-27 11:20:56 +0200

    more and more

---
+++ brocode/main.py
@@ -0,0 +1,46 @@
+# # import  time
+# #
+# # # website = "http://google.com"
+# # # web_slice=slice(7,-4)
+# # # print(website[web_slice])
+# # # raise nam
+# # # name=None
+# # # while not name or name.isdigit():
+# # #     name = input("enter your name ")
+# # #
+# # # print(name)
+# #
+# # # for seconds in range(10,0,-1):
+# # #     print(seconds)
+# # #     time.sleep(1)
+# # # print('hey bro')
+# # rows=int(input("how many rows?: "))
+# # columns=int(input("how many columns?: "))
+# # symbol=input("which symbol to use?:")
+# # for i in range(rows):
+# #     for j in range(columns):
+# #         print(symbol,end="")
+# #     print()
+# #     def hello(first,middle,last):
+# #         print("Hello"+first+" "+middle+" "+last)
+# #     hello(last='kevin',middle="kubwayo",first="k")
+# # print(round(abs(float(input("enter the number")))))
+# # nested functions
+# # def add(*args):
+# #     # tuple
+# #     args=list(args)
+# #     args[0]=30
+# #     sum=0
+# #     for i in args:
+# #         sum+=i;
+# #     return sum
+# # print(add(1,23,10))
+# # dictionary
+#
+# def hello(**kwargs):
+#     print("Hello",end=" ")
+#     for key,value in kwargs.items():
+#         print(value,end=" ")
+# hello(title="Coder",first="Bigirabagabo",middle="patience",last="Blaise")
+
+str.format()
